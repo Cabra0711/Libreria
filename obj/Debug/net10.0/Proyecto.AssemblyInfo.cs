a1bf864426f388a8c51cb99ff1da98e966ae5cf4
Cabra0711 <juanescabreraperez@gmail.com>
2026-04-26 19:33:47 -0500

    agg dockerfile

--- obj/Debug/net10.0/Proyecto.AssemblyInfo.cs
+++ obj/Debug/net10.0/Proyecto.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Proyecto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+315b0bf4aa0f712751dbbd3c4f905a710f1bfb75")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e960b7952f05a0f20d86e0cc1ffbe7d9ad7c66d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Proyecto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Proyecto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
